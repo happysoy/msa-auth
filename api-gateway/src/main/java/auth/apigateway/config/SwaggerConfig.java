7b945b2a7ac4f34ceee2bc1663c101b4bb91a81d
happysoy <jeongsoyeon0130@naver.com>
2023-12-21 22:55:48 +0900

    [feat] filter 추가

--- api-gateway/src/main/java/auth/apigateway/config/SwaggerConfig.java
+++ api-gateway/src/main/java/auth/apigateway/config/SwaggerConfig.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+@Primary
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "API Documentation", version = "1.0"))
 public class SwaggerConfig {
--- api-gateway/src/main/java/auth/apigateway/util/JwtValidationUtils.java
+++ api-gateway/src/main/java/auth/apigateway/util/JwtValidationUtils.java
@@ -1,2 +1,36 @@
-package auth.apigateway.util;public class JwtValidationUtils {
+package auth.apigateway.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+@RequiredArgsConstructor
+public class JwtValidationUtils {
+
+    private final static Key secret_key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+
+    public static String getRoleByToken(String token) {
+        return (String) Jwts.parserBuilder()
+                .setSigningKey(secret_key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role");
+    }
+
+    public static String getEmailByToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret_key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+
 }
