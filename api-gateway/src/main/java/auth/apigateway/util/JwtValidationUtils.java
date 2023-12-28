e7e0298344c5613ef369c3aa52890a6f788fead5
happysoy <jeongsoyeon0130@naver.com>
2023-12-28 18:14:11 +0900

    [fix] secret key 환경변수로 변경

--- api-gateway/src/main/java/auth/apigateway/util/JwtValidationUtils.java
+++ api-gateway/src/main/java/auth/apigateway/util/JwtValidationUtils.java
@@ -3,8 +3,6 @@ package auth.apigateway.util;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -24,12 +22,9 @@ public class JwtValidationUtils {
         this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
     }
 
-//    private final static Key secret_key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-    private static Environment env;
 
     public String getRoleByToken(String token) {
         return (String) Jwts.parserBuilder()
-//                .setSigningKey(publicKeyEndpoint)
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token)
